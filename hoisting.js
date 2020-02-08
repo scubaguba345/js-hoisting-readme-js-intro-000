05073e059a1cd547d066b06c7535a82be2821dc5
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 14:13:43 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -19,17 +19,28 @@ function thisIsCrazy() {
   }
 }
 }
-
 function sayMyName() {
+//   // fix the code in here:
+//   var name = "Cricky";
+//
+//   function sayMy() {
+//     name = "Kristin"; // took out var before name here
+//     console.log(name);
+//   }
+//   sayMy();
+// }
+// sayMyName()()
+
+var sayMyName = function() {
   // fix the code in here:
-  var name = "Cricky";
+  var name;
+  name = "Cricky";
 
-  sayMy();
   function sayMy() {
-    name ="kristin";
+    name = "Kristin"; // took out var before name here
     console.log(name);
-
-    sayMy();
-
+    var name = "Kristin";
   }
+  sayMy();
 }
+sayMyName()()
